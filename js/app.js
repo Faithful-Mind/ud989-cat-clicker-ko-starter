94bfad229d1bfef1ed6bd22efb61719e277cd1b5
Faithful-Mind <lpyy15@gmail.com>
2018-05-30 20:42:37 +0800

    Separate Model and ViewModel

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
-var ViewModel = function () {
+var Cat = function () {
   this.name = ko.observable('Dragon Li');
   this.imgSrc = ko.observable('https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426');
   this.clickCount = ko.observable(0);
   this.visible = ko.observable(false);
-
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  };
 
   this.level = ko.computed(function() {
     switch (parseInt(this.clickCount() / 10)) {
@@ -20,6 +16,18 @@ var ViewModel = function () {
         break;
     }
   }, this);
+
+  this.nicknames = ko.observableArray(['Kiiten', 'Catty', 'Garfield']);
+
+}
+
+var ViewModel = function () {
+  this.currentCat = ko.observable(new Cat());
+
+  this.incrementCounter = function() {
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+  };
+
 };
 
 ko.applyBindings(new ViewModel());
